ceaab0846a9a930ac02d7f188700c6dd362d06a5
Brawcks <coffin.vincent.sp@gmail.com>
2017-07-13 12:10:47 +0200

    ajout de packages et suppression du index

---
+++ module.js
@@ -0,0 +1,10 @@
+var direBonjour = function() {
+    alert('salut');
+}
+
+var direByeBye = function() {
+    alert('salut');
+}
+
+exports.direBonjour = direBonjour;
+exports.direByeBye = direByeBye;
